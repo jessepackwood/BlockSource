dfa28ab53038c56bee4daaad5fc15b3bf3461d7c
David Ryan <djrdev@icloud.com>
2018-02-14 23:27:11 -0700

    edited App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 import './App.css';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header></header>
+      <div className="app">
+        <header><p>Blocksource</p></header>
         <main></main>
       </div>
     );
