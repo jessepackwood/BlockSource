c7ced18e2fe48e117d74e63064c944103ebaf876
David Ryan <djrdev@icloud.com>
2018-02-14 23:26:09 -0700

    removed Router from index.js file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from './Components/App/App';
 import { Provider } from 'react-redux';
 import Store from './Store';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
