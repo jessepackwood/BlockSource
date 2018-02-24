c8ed0396b8d876e5bb7b1dd17a2cbc169521ebe5
jessepackwood <packwoodjesse@gmail.com>
2018-02-24 11:54:19 -0700

    create Header test suite

---
+++ src/containers/Header/Header.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import { shallow } from 'enzyme';
+
+describe('Header tests', () => {
+  let header;
+  beforeEach( () => {
+    header = shallow(<Header/>)
+  })
+  it('should be defined', () => {
+    expect(header).toBeDefined()
+  })
+
+})
