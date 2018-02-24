aac6f679cd0fa5d90f72a3f2f2c2db2c837f50c4
jessepackwood <packwoodjesse@gmail.com>
2018-02-24 11:55:31 -0700

    configure firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase';
 
-
+const config = {
+  apiKey: "AIzaSyAZPZ97h6Wy2zrFVEGSwBygvFP4JtR4--s",
+  authDomain: "blocksource-3436a.firebaseapp.com",
+  databaseURL: "https://blocksource-3436a.firebaseio.com",
+  projectId: "blocksource-3436a",
+  storageBucket: "blocksource-3436a.appspot.com",
+  messagingSenderId: "208434052605"
+};
 
 firebase.initializeApp(config);
 
