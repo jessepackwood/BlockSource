7aef21dbfb3238eab6e2e6f1ff6278f4af585975
jessepackwood <packwoodjesse@gmail.com>
2018-02-26 13:55:07 -0700

    rendering material card

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -13,7 +13,7 @@ export class Header extends Component {
           <button className='button'>Start a project</button>
         </Link>
         <Link to ='/'>
-          <h1 className='app-title'>TEST</h1>
+          <h1 className='app-title'>BlockSource</h1>
         </Link>
         <h4 className='app-subtitle'>Crowdfunding on the blockchain</h4>
         <div className='search-login-container'>
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import './Projects.css';
+import FlatButton from 'material-ui/FlatButton';
 
 class Projects extends Component {
+
+  componentDidMount() {
+    //fetch projects
+    //map over projects and return cards
+  }
+
   render() {
     return (
       <div className='projects-component'>
-        Projects
-        <Card/>
+        <Card>
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+          </CardActions>
+        </Card>
       </div>
     )
   }
