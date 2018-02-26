c1bfad5bc1ba5024273bee13208af1069e828d2c
David Ryan <djrdev@icloud.com>
2018-02-26 15:04:04 -0700

    made the new seed work

---
+++ db/seeds/projectsContributorsData.js
@@ -0,0 +1,238 @@
+// projectsContributorsData.js
+
+module.exports = [
+  {
+    "projects_id": "1",
+    "contributors_id": "1",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "2",
+    "contributors_id": "1",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "3",
+    "contributors_id": "1",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "4",
+    "contributors_id": "1",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "5",
+    "contributors_id": "2",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "1",
+    "contributors_id": "2",
+    "owner": "false",
+    "contribution_amount": "20000.0"
+  },
+  {
+    "projects_id": "2",
+    "contributors_id": "3",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "2",
+    "contributors_id": "4",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "3",
+    "contributors_id": "1",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "3",
+    "contributors_id": "5",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "3",
+    "contributors_id": "5",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "4",
+    "contributors_id": "5",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "4",
+    "contributors_id": "4",
+    "owner": "false",
+    "contribution_amount": "10000.0"
+  },
+  {
+    "projects_id": "5",
+    "contributors_id": "3",
+    "owner": "false",
+    "contribution_amount": "20000.0"
+  },
+  {
+    "projects_id": "6",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "7",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "8",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "9",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "10",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "11",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "12",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "13",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "14",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "15",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "16",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "17",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "18",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "19",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "20",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "21",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "22",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "23",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "24",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "25",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "26",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "27",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "28",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "29",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  },
+  {
+    "projects_id": "30",
+    "contributors_id": "3",
+    "owner": "true",
+    "contribution_amount": "0.0"
+  }
+];
