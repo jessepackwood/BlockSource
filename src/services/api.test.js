a58ca36f6820a351aa14efc8551ec151928c8e05
jessepackwood <packwoodjesse@gmail.com>
2018-02-27 16:55:47 -0700

    retouch styling for projectForm

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -17,6 +17,23 @@ describe('Api Calls tests', () => {
     });
   });
 
+  describe('Projects post test', () => {
+    it('should post a new project', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(
+          {
+            title: 'Project 1',
+            description: 'the best project',
+            goal_amount: '5000',
+            fund_amount: '2000'
+          }
+        )
+      }));
+      const newProject = await apiCalls.postProject({project: 'payload'});
+      expect(newProject).toEqual({"description": "the best project", "fund_amount": "2000", "goal_amount": "5000", "title": "Project 1"});
+    });
+  })
+
   describe('Contributors fetch test', () => {
     beforeEach(() => {
       window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
