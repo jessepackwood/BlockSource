2900359c69efa5a1c9344ec8144d3d0b72671325
jessepackwood <packwoodjesse@gmail.com>
2018-02-27 10:56:09 -0700

    add function for fetching project contributors

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardTitle, CardText} from 'material-ui/Card';
 import './Projects.css';
 import { connect } from 'react-redux';
-import { projectsEndpoint } from '../../actions';
+import { projectsEndpoint, contributorsEndpoint } from '../../actions';
 import FlatButton from 'material-ui/FlatButton';
 
 export class Projects extends Component {
@@ -10,18 +10,22 @@ export class Projects extends Component {
     this.props.handleProjectsFetch()
   }
 
-
   mapProjects() {
+    // const contributors = this.props.contributors.length ? <p>{this.props.contributors}</p> : null;
     return this.props.projects.map( (project, index) => {
       return <Card key={index}>
                 <CardHeader
                   title={project.title}
                   subtitle={`Goal Amount: ${project.goal_amount} Fund Amount: ${project.fund_amount}`}
-                  actAsExpander={true}
-                  showExpandableButton={true}
                 />
-                <CardText expandable={true}>
+                <CardText>
                   {project.description}
+                </CardText>
+                <CardActions actAsExpander={true}>
+                  <FlatButton label="Contributors" onClick={() => this.props.showContributors(project.id)}/>
+                </CardActions>
+                <CardText expandable={true}>
+                  
                 </CardText>
               </Card>
       })
@@ -38,13 +42,18 @@ export class Projects extends Component {
 }
 
 export const mapStateToProps = store => ({
-  projects: store.projects
+  projects: store.projects,
+  contributors: store.projectContributors
 })
 
 export const mapDispatchToProps = (dispatch) => {
   return {
     handleProjectsFetch: () => {
       dispatch(projectsEndpoint())
+    },
+    showContributors: (id) => {
+
+      dispatch(contributorsEndpoint(id))
     }
   }
 }
